1f009847f3bddb35cf6443c1074591540fc80629
Cloud Mario <smcz@qq.com>
2015-10-08 15:49:43 +0800

    fix

--- bash/hehe-preseed.sh
+++ bash/hehe-preseed.sh
@@ -7,7 +7,7 @@ DOCKER_PULL="docker pull"
 COMPONENTS=(store-monitor store-metadata store-gateway store-daemon router registry publisher logspout logger database controller builder cache store-admin mesos-master mesos-slave mesos-marathon zookeeper)
 for c in "${COMPONENTS[@]}"; do
     image=deis/${c}:$DEIS_RELEASE
-    docker history $image >/dev/null 2>&1 || docker ${DOCKER_PULL} $image
+    docker history $image >/dev/null 2>&1 || ${DOCKER_PULL} $image
     docker tag -f ${image} ${BACKEND_HOST}/${image} && docker push ${BACKEND_HOST}/${image}
 done
 
@@ -15,13 +15,13 @@ done
 COMPONENTS_QUAY_IO=(coreos/etcd:v2.1.1 coreos/flannel:0.5.1 coreos/discovery.etcd.io)
 for c in "${COMPONENTS_QUAY_IO[@]}"; do
     image=quay.io/${c}
-    docker history $image >/dev/null 2>&1 || docker ${DOCKER_PULL} $image
+    docker history $image >/dev/null 2>&1 || ${DOCKER_PULL} $image
     docker tag -f ${image} ${BACKEND_HOST}/${c} && docker push ${BACKEND_HOST}/${c}
 done
 
 COMPONENTS_HEROKU=(cedar:14)
 for c in "${COMPONENTS_HEROKU[@]}"; do
     image=heroku/${c}
-    docker history $image >/dev/null 2>&1 || docker ${DOCKER_PULL} $image
+    docker history $image >/dev/null 2>&1 || ${DOCKER_PULL} $image
     docker tag -f ${image} ${BACKEND_HOST}/${image} && docker push ${BACKEND_HOST}/${image}
 done
