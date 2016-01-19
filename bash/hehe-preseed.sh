2f661242479f20c8d7753f4e9f736904e0290fb2
科学家 <smcz@qq.com>
2016-01-19 15:16:38 +0800

    使用daocloud.io存储cedar:14

--- bash/hehe-preseed.sh
+++ bash/hehe-preseed.sh
@@ -2,7 +2,7 @@
 
 DEIS_RELEASE=v1.12.1
 BACKEND_HOST=127.0.0.1:5000;
-DOCKER_PULL="docker pull"
+DOCKER_PULL="dao pull"
 
 COMPONENTS=(store-monitor store-metadata store-gateway store-daemon router registry publisher logspout logger database controller builder cache store-admin mesos-master mesos-slave mesos-marathon zookeeper)
 for c in "${COMPONENTS[@]}"; do
@@ -23,5 +23,6 @@ COMPONENTS_HEROKU=(cedar:14)
 for c in "${COMPONENTS_HEROKU[@]}"; do
     image=heroku/${c}
     docker history $image >/dev/null 2>&1 || ${DOCKER_PULL} $image
-    docker tag -f ${image} ${BACKEND_HOST}/${image} && docker push ${BACKEND_HOST}/${image}
+    # docker tag -f ${image} ${BACKEND_HOST}/${image} && docker push ${BACKEND_HOST}/${image}
+    docker tag -f ${image} daocloud.io/cloudmario/${c} && docker push daocloud.io/cloudmario/${c}
 done
